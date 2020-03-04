0644307a3a9acb51acc825e0a78d20905e7dae02
arowana <arowana.java@qq.com>
2020-03-05 02:53:12 +0800

    :wrench: 使用slf4j

--- douyin-downloader/src/main/java/com/aries/crawler/Starter.java
+++ douyin-downloader/src/main/java/com/aries/crawler/Starter.java
@@ -3,12 +3,20 @@ package com.aries.crawler;
 import com.aries.crawler.verticles.DatabaseVerticle;
 import com.aries.crawler.verticles.ProducerVerticle;
 import io.vertx.core.*;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 /**
  * @author arowana
  */
 public class Starter {
+    private static final Logger logger = LoggerFactory.getLogger(Starter.class);
+
     public static void main(String[] args) {
+        //Force to use slf4j
+        // 参考自dgate：https://github.com/DTeam-Top/dgate/blob/master/src/main/java/top/dteam/dgate/Launcher.java
+        System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.SLF4JLogDelegateFactory");
+
         Vertx vertx = Vertx.vertx();
 
         Future<Void> producerFuture = Future.future(res -> {
